5fdc2ddc75a125cb6f5542a3c04aac02cf38bed8
Dylan <dylanlozano439@gmail.com>
2025-04-09 01:11:20 -0500

    index,primer nombre y archi.js para mostrar nombres en el index

---
+++ script.js
@@ -0,0 +1,19 @@
+const archivos = [
+    'dato1.json'
+  ];
+  
+  const lista = document.getElementById('lista');
+  
+  archivos.forEach(archivo => {
+    fetch(archivo)
+      .then(res => res.json())
+      .then(data => {
+        const li = document.createElement('li');
+        li.textContent = data.nombre;
+        lista.appendChild(li);
+      })
+      .catch(error => {
+        console.error(`Error al cargar ${archivo}:`, error);
+      });
+  });
+  
